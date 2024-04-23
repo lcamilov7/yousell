0f20ab87882e96127685cebbee1329a741d23882
Luis Camilo <luiscamilov@hotmail.com>
2024-04-23 16:05:12 -0500

    before inmportmap for alert and flashes

--- app/javascript/controllers/button_menu_controller.js
+++ app/javascript/controllers/button_menu_controller.js
@@ -2,13 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="button-menu"
 export default class extends Controller {
-  static targets = ['button', 'menu']
-  connect() {
-    console.log(this.buttonTarget);
-    console.log(this.menuTarget);
-  }
+  static targets = ['menu']
 
-  fire() {
+  toggle() {
     this.menuTarget.classList.toggle('translate-x-full');
   }
 }
